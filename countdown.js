2a2f26324a11273124dcbcee7dc526454c2efb6a
Patrick Vieira Feitosa <patrick.vieira@enext.local>
2018-12-22 17:44:07 -0200

    First Commit - Repository criation

---
+++ countdown.js
@@ -0,0 +1,47 @@
+export default class Countdown {
+    constructor(futureDate) {
+        this.futureDate = futureDate;
+    }
+
+    get _timeStampDiff() {
+        return this._futureDate.getTime() - this._currentDate.getTime();
+    }
+
+    get _currentDate() {
+        return new Date();
+    }
+
+    get _futureDate() {
+        return new Date(this.futureDate);
+    }
+
+    get days() {
+        return Math.floor(this._timeStampDiff / (24 * 60 * 60 * 1000));
+    }
+
+    get hours() {
+        return Math.floor(this._timeStampDiff / (60 * 60 * 1000));
+    }
+
+    get minutes() {
+        return Math.floor(this._timeStampDiff / (60 * 1000));
+    }
+
+    get seconds() {
+        return Math.floor(this._timeStampDiff / 1000);
+    }
+
+    get result() {
+        const days = this.days;
+        const hours = this.hours % 24;
+        const minutes = this.minutes % 60;
+        const seconds = this.seconds % 60;
+
+        return {
+            days,
+            hours,
+            minutes,
+            seconds,
+        }
+    }
+}
---
+++ script.js
@@ -0,0 +1,5 @@
+import Countdown from './countdown.js';
+
+const daysToXmas = new Countdown('24 December 2018 23:59:59 GMT-0300');
+
+console.log(daysToXmas);
